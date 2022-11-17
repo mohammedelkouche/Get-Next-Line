3a37028219eabd86e0a31f9e8e571c5a511157fe
mohammedelkouche <mohammed0elkouche@gmail.com>
2022-11-18 00:01:46 +0100

    fefine in header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:19:39 by mel-kouc          #+#    #+#             */
-/*   Updated: 2022/11/16 12:20:21 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2022/11/17 22:25:27 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
