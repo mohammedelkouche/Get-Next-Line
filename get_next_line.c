8fb15f78864cf43fb3486d82449be6f84eee8aec
mohammedelkouche <mohammed0elkouche@gmail.com>
2022-11-18 20:28:17 +0100

    add variable in fuction

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,25 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:19:32 by mel-kouc          #+#    #+#             */
-/*   Updated: 2022/11/17 09:35:47 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2022/11/18 19:38:05 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*getline(int fd)
+{
+	char	*buffer;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static int	atorage;
+	char		*line;
+
+	line = getline(fd);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,19 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:19:39 by mel-kouc          #+#    #+#             */
-/*   Updated: 2022/11/17 22:25:27 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2022/11/18 19:37:14 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
+
+# ifdef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
 
 char	*get_next_line(int fd);
 
